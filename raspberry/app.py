2ed102a809383c0d413d3f6b45b402afb3832514
Guilherme Müller <30841186+gmullernh@users.noreply.github.com>
2022-06-26 12:02:47 -0300

    Update app.py
    add support form JSON content

--- raspberry/app.py
+++ raspberry/app.py
@@ -1,22 +1,40 @@
+# DHT11
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
-from flask import Flask
 
-# Sensor setup
-sensor = Adafruit_DHT.DHT11
 GPIO.setmode(GPIO.BOARD)
- 
-# GPIO where the sensor will be connected
-sensorPin = 2
 
-# Creates a webserver
+# Defines the sensor and it's pin
+sensor = Adafruit_DHT.DHT11
+sensor_pin = 2 # this must match with the pin you connected the data wire.
+
+# Add webserver
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import json
+from datetime import date, datetime
+
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/metrics')
-def metrics():
-    umid, temp = Adafruit_DHT.read_retry(sensor, sensorPin)
+@app.route('/api', methods=['GET'], defaults={'dataFormat': None})
+@app.route('/api/<string:dataFormat>', methods=['GET'])
+@cross_origin()
+def metrics(dataFormat):
+    umid, temp = Adafruit_DHT.read_retry(sensor, sensor_pin)
+    x = { "temperature" : temp, "humidity": umid, "datetime": datetime.now().isoformat() }
     if umid is not None and temp is not None:
-        return '# HELP local_temp local temperature\n# TYPE local_temp gauge\nlocal_temp {}\n# HELP local_umid local umid\n# TYPE local_umid gauge\nlocal_umid {}\n'.format(int(temp), int(umid)), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+        if dataFormat is not None and dataFormat == "json":
+            return json.dumps(x, default=json_serial), 200, {'Content-Type': 'application/json; charset=utf-8'}
+        return '# HELP local_temp local temperature\n# TYPE local_temp gauge\nlocal_temp {}\n# HELP local_humidity local humidity\n# TYPE local_humidity gauge\nlocal_humidity {}\n'.format(int(temp), int(umid)), 200, {'Content-Type': 'text/plain; charset=utf-8'}
     else:
-        return 'Couldn\'t read data from DHT11 !!!', 200, {'Content-Type': 'text/plain; charset=utf-8'}
+        return 'Could not read from DHT11.', 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+# https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
